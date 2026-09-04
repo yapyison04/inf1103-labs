3bb2e48a563c1a2e0813db61548f35015d505a63
Yap Yison <2605764@sit.singaporetech.edu.sg>
2026-09-05 01:18:29 +0800

    third post

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -2,3 +2,33 @@ print("================================")
 print("Welcome here")
 print("My first post!")
 print("================================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+

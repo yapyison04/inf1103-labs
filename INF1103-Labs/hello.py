0c2a78403fdec35d38c36ebaf10649850c5c412d
Yap Yison <2605764@sit.singaporetech.edu.sg>
2026-09-05 01:21:52 +0800

    fourth post

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -22,7 +22,7 @@ print("Followers:", followers)
 
 
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
@@ -31,4 +31,5 @@ print("Username:", username)
 print("Age:", age)
 print("Category:", category)
 
-
+if age > 40 and category == "fun":
+    print("You are old what is fun for you??")
